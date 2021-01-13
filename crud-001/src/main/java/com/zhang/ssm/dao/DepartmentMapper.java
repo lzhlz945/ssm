4d7e875e9a0c31734b51bd7ac031c21e788e30d4
Administrator <1746678613@qq.com>
2021-01-13 13:55:39 +0800

    bran1 -ssm 逆向工程完毕

---
+++ crud-001/src/main/java/com/zhang/ssm/dao/DepartmentMapper.java
@@ -0,0 +1,17 @@
+package com.zhang.ssm.dao;
+
+import com.zhang.ssm.bean.Department;
+
+public interface DepartmentMapper {
+    int deleteByPrimaryKey(Integer deptId);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    Department selectByPrimaryKey(Integer deptId);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
---
+++ crud-001/src/main/java/com/zhang/ssm/dao/EmployeeMapper.java
@@ -0,0 +1,17 @@
+package com.zhang.ssm.dao;
+
+import com.zhang.ssm.bean.Employee;
+
+public interface EmployeeMapper {
+    int deleteByPrimaryKey(Integer empId);
+
+    int insert(Employee record);
+
+    int insertSelective(Employee record);
+
+    Employee selectByPrimaryKey(Integer empId);
+
+    int updateByPrimaryKeySelective(Employee record);
+
+    int updateByPrimaryKey(Employee record);
+}
